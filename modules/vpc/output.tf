979ff862b6d0dc3566a80d6e5796a334349ac99f
hungacmaa <hungkojno1@gmail.com>
2023-10-18 00:49:33 +0700

    update output file in vpc module

--- modules/vpc/output.tf
+++ modules/vpc/output.tf
@@ -1,3 +1,23 @@
 output "vpc_id" {
-  value = aws_vpc.shopzer-vpc
+  value = aws_vpc.shopzer-vpc.id
+}
+
+output "default_security_group_id" {
+  value = aws_vpc.shopzer-vpc.default_security_group_id
+}
+
+output "public_subnet_ids" {
+  value = [for subnet in aws_subnet.public_subnet : subnet.id]
+}
+
+output "frontend_subnet_ids" {
+  value = [for subnet in aws_subnet.frontend_subnet : subnet.id]
+}
+
+output "backend_subnet_ids" {
+  value = [for subnet in aws_subnet.backend_subnet : subnet.id]
+}
+
+output "database_subnet_ids" {
+  value = [for subnet in aws_subnet.database_subnet : subnet.id]
 }
