c1e53dbcb42c8a822e08be89e70be0476cadbb3d
hungacmaa <hungkojno1@gmail.com>
2023-10-19 00:28:46 +0700

    update variable inputs for instances module

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -21,3 +21,33 @@ variable "backend_subnet_ids" {
 variable "database_subnet_ids" {
   type = list(string)
 }
+
+variable "vpc_cidr" {
+  description = "VPC CIDR"
+  type        = string
+  default     = "172.20.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "Public subnet CIDR"
+  type        = list(string)
+  default     = ["172.20.1.0/24", "172.20.2.0/24"]
+}
+
+variable "internet_cidr" {
+  description = "cidr block for internet"
+  type        = string
+  default     = "0.0.0.0/0"
+}
+
+variable "ssh_key_name" {
+  type    = string
+  default = "keypair-l1"
+}
+
+variable "frontend_subnet_cidrs" {
+  description = "Subnet for frontend"
+  type        = list(string)
+  default     = ["172.20.3.0/24", "172.20.4.0/24"]
+
+}
