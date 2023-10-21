f472a3bbc91feec09ad3d347035095dcfca3ee6b
hungacmaa <hungkojno1@gmail.com>
2023-10-22 01:02:53 +0700

    output for load balancers

--- main.tf
+++ main.tf
@@ -19,3 +19,4 @@ module "elb" {
   depends_on = [module.vpc]
 
 }
+
--- modules/load_balancer/output.tf
+++ modules/load_balancer/output.tf
@@ -1,1 +1,17 @@
 # Output for load balancer module
+
+output "be_alb_id" {
+  value = aws_lb.be_alb.id
+}
+
+output "fe_alb_id" {
+  value = aws_lb.fe_alb.id
+}
+
+output "be_dns_name" {
+  value = aws_lb.be_alb.dns_name
+}
+
+output "fe_dns_name" {
+  value = aws_lb.fe_alb.dns_name
+}
