1e7dc2a72cc529d3a9e7ebe35a73beb663f1e486
hungacmaa <hungkojno1@gmail.com>
2023-10-22 00:21:55 +0700

    coding backend alb resource

--- modules/load_balancer/variables.tf
+++ modules/load_balancer/variables.tf
@@ -1,1 +1,24 @@
 # Input variables for load balancer module
+variable "vpc_id" {
+  type    = string
+  default = "172.20.0.0/16"
+}
+
+variable "lb_type" {
+  type    = string
+  default = "application"
+}
+
+variable "backend_subnet_cidrs" {
+  type    = list(string)
+  default = ["172.20.5.0/24", "172.20.6.0/24"]
+}
+
+variable "public_subnet_cidrs" {
+  type    = list(string)
+  default = ["172.20.1.0/24", "172.20.2.0/24"]
+}
+
+variable "public_subnet_ids" {
+  type = list(string)
+}
